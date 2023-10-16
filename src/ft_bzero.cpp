2306b1271b4d7c2aee28d40a70e43273b5039e1d
rudy deceliere <61892880+deceliere@users.noreply.github.com>
2023-10-17 01:42:47 +0200

    remove copyFile (not working)

--- src/ft_bzero.cpp
+++ src/ft_bzero.cpp
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_bzero.cpp                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: r <r@student.42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/24 17:54:08 by rdecelie          #+#    #+#             */
-/*   Updated: 2023/10/17 00:57:56 by r                ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "pavillon.h"
 
@@ -26,29 +15,4 @@ void	ft_bzero(unsigned char *s, size_t n)
 		txt[a] = 0;
 		a++;
 	}
-}
-
-
-bool copyFileToSD(const char* sourceFileName, const char* targetFileName) {
-  File sourceFile = SD.open(sourceFileName);
-  if (!sourceFile) {
-    Serial.println("Impossible d'ouvrir le fichier source.");
-    return false;
-  }
-
-  File targetFile = SD.open(targetFileName, FILE_WRITE);
-  if (!targetFile) {
-    Serial.println("Impossible d'ouvrir le fichier cible.");
-    sourceFile.close();
-    return false;
-  }
-
-  while (sourceFile.available()) {
-    char data = sourceFile.read();
-    targetFile.write(data);
-  }
-
-  sourceFile.close();
-  targetFile.close();
-  return true;
 }
